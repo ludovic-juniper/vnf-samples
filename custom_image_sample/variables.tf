e60a4c4e7479f98a58bcf7398202247cf79ccda5
ludovic-juniper <ludovic@juniper.net>
2021-06-28 10:18:50 +0200

    Update variables.tf
    first change

--- custom_image_sample/variables.tf
+++ custom_image_sample/variables.tf
@@ -8,15 +8,16 @@
 #                             onbaording time.This variable is hidden from the user.
 ##############################################################################
 variable "vnf_cos_image_url" {
-  default     = "cos://us-south/cos-bucket/image.qcow2"
-  description = "The COS image object SQL URL for qcow2 image."
+#  default     = "cos://ap-geo/asloma-juniper-bucket/junos-vsrx3-x86-64-19.4R3.11.qcow2"
+  default     = "cos://eu-de/vsrx-eu-de-cos-bucket/junos-vsrx3-x86-64-21.1R1.11.qcow2"
+  description = "The COS image object SQL URL for vSRX 19,4R3.11 qcow2 image."
 }
 
 ##############################################################################
 # vnf_vpc_image_name - The name of the Custom image to be provisioned in your IBM Cloud account.
 ##############################################################################
 variable "vnf_vpc_image_name" {
-  default     = "test-custom-img"
+  default     = "ajsloma-juniper-vsrx3-194"
   description = "The name of the Custom image to be provisioned in your IBM Cloud account."
 }
 
@@ -39,7 +40,7 @@ variable "image_operating_system" {
 ######################################################################################################
 /*
 variable "api_key" {
-  default     = ""
+  default     = "c2cbcb5ab2e6440c93810f3df7a74e94"
   description = "holds the user api key"
 }*/
 
