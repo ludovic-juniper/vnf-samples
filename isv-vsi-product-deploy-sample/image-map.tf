865c48a67dec7082fa4465e8e1ea336e24aa06dc
ludovic-juniper <ludovic@juniper.net>
2021-06-29 11:32:41 +0200

    added isv-vsi-product-deploy-sample

---
+++ isv-vsi-product-deploy-sample/image-map.tf
@@ -0,0 +1,12 @@
+locals {
+        image_map = {
+			"us-south" = "r006-19224a9f-9bc1-40a6-8560-c0840581e12d"
+			"us-east" = "r014-426f77ab-0f97-4b78-b25a-5f5071613154"
+			"eu-gb" = "r018-2e9d1f29-f93a-4f4f-b067-73a91cd4a012"
+			"eu-de" = "r010-034e7325-a29f-4f22-8c30-62e8c2de5f74"
+			"jp-tok" = "r022-4f9ea516-a88d-4038-b28d-d96fa48576f1"
+			"jp-osa" = "r034-eafa2421-fc28-4be8-ae4f-2d15232df68b"
+			"au-syd" = "r026-22bbc3df-b017-48f5-b627-7c0045000d11"
+#			"ca-tor" = "unvailable"
+        }
+}
